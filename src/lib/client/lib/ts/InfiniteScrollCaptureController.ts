dcdaf87b1a522311eb59c3a35a7d5929794d5a55
Matthew <863968+mattbis@users.noreply.github.com>
2024-10-12 02:40:38 +0100

    Update InfiniteScrollCaptureController.ts

---
+++ src/lib/client/lib/ts/InfiniteScrollCaptureController.ts
@@ -0,0 +1,4 @@
+/* determine the extent of the current viewport, inject/simulate the scroll down to determine a new page...
+a new page is sent to the PageController */
+export InfiniteScrollCaptureController {
+}
